06dc598820b4d142d5b6045a7c65838f22350a35
John <johnoptic@proton.me>
2024-10-09 10:11:32 +0100

    python assignment

---
+++ assignment.py
@@ -0,0 +1,46 @@
+# Multiple Assignment Statements
+#-------------------------------
+
+# you can assign multiple variables and their values on the same line
+x,y,z = 10,20,30
+print(x)
+print(y)
+print(z)
+
+# you can also assign multiple variables the same values on the same line
+a = b = c = 4
+print(a)
+print(b)
+print(c)
+
+# f-strings
+
+name = "john"
+age = "34"
+
+print(f"His name is {name} and he is {age} years old")
+
+# Practise Time
+print("Practise Time")
+print("-------------")
+
+a = 10
+b = 20 
+c = 30
+
+masonicNumber = 33
+transportType = "car"
+myFloat = 2.158
+greeting = "hey"
+
+print(masonicNumber)
+print(transportType)
+print(myFloat)
+print(greeting)
+
+dave, bob, mark = 41, 22, 38
+
+ages = {"Dave":'41',"Bob":'22', "Mark":'38'}
+for key, value in ages.items():
+    print(f"{key} is {value} years old")
+
