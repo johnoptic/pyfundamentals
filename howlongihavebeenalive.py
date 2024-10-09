e812ca9c789f9e33e1f8b605356b500d3f98eb05
John <johnoptic@proton.me>
2024-10-09 10:19:31 +0100

    update readme

---
+++ howlongihavebeenalive.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+time_now = datetime.now()
+print('This is Todays date: ')
+print(time_now)
+
+how_long_have_i_lived = datetime.now() - datetime(1989, 3, 27)
+print('This is how long you have lived: ')
+print(how_long_have_i_lived)
+
+
+
+
