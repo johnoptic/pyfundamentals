feff3c3a6ca5eaf7826a73105dd74a0714007c4a
John <johnoptic@proton.me>
2024-11-05 09:38:28 +0000

    add boolean logic

---
+++ bool.py
@@ -0,0 +1,27 @@
+print("Breakdown of Boolean Logic")
+print("--------------------------")
+
+print("True AND True")
+print(True and True)
+print("--------------------------")
+print("\n")
+
+print("True OR False")
+print(True or False)
+print("--------------------------")
+print("\n")
+
+print("True AND False")
+print(True and False)
+print("--------------------------")
+print("\n")
+
+print("False AND False")
+print(False and False)
+print("--------------------------")
+print("\n")
+
+print("False OR False")
+print(False or False)
+print("--------------------------")
+print("\n")
